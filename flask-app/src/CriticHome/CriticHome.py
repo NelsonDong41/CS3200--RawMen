82ee04b90d893b0bc019e61cff309d244bba5e2a
Aidan Domondon <93887720+aidandomondon@users.noreply.github.com>
2022-12-09 18:15:19 -0500

    Update CriticHome.py

--- flask-app/src/CriticHome/CriticHome.py
+++ flask-app/src/CriticHome/CriticHome.py
@@ -5,7 +5,7 @@ from src import db
 CriticHome = Blueprint('CriticHome', __name__)
 
 #display the first and last name of the critic with id <critic_id>.
-@CriticHome.route('/name/<critic_id>', methods=["GET"])
+@CriticHome.route('/<critic_id>/name', methods=["GET"])
 def get_name(critic_id):
     cursor = db.get_db().cursor()
     cursor.execute(f'SELECT Critic.first_name, Critic.last_name \
@@ -21,12 +21,36 @@ def get_name(critic_id):
     the_response.mimetype = 'application/json'
     return the_response
 
-@CriticHome.route('/company/<critic_id>', methods=["GET"])
+# gets the publication company under which this reviewer has written the most reviews
+@CriticHome.route('/<critic_id>/company', methods=["GET"])
 def get_company(critic_id):
     cursor = db.get_db().cursor()
-    cursor.execute(f'SELECT Critique.p, Critic.last_name \
+    query = ''' SELECT publication_company \
+                    FROM (SELECT publication_company, MAX(n) \
+                            FROM (SELECT publication_company, COUNT(Critique.critique_id) as n \
+                                    FROM Critic JOIN Critique ON (Critic.critic_id = Critique.critic_id) \
+                                    WHERE Critic.critic_id = {critic_id} \
+                                    GROUP BY publication_company))
+    '''
+    cursor.execute(query)
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+# gets all past reviews
+@CriticHome.route('/<critic_id>/past_reviews', methods=["GET"])
+def get_past_reviews(critic_id):
+    cursor = db.get_db().cursor()
+    cursor.execute(f'SELECT Critique.publication_company, Critique.ramen_id, Product.name \
     FROM Critic \
     JOIN Critique ON (Critic.critic_id = Critique.critic_id) \
+    JOIN Product on (Critique.ramen_id = Product.ramen_id) \
     WHERE Critic.critic_id = {critic_id}')
     row_headers = [x[0] for x in cursor.description]
     json_data = []
@@ -38,29 +62,14 @@ def get_company(critic_id):
     the_response.mimetype = 'application/json'
     return the_response
 
-@CriticHome.route('/recent_reviews/<critic_id>', methods=["GET"])
-def get_recent_reviews(critic_id):
+# gets the highest reviews this critic has given
+@CriticHome.route('/<critic_id>/highest_reviews', methods=["GET"])
+def get_highest_reviews(critic_id):
     cursor = db.get_db().cursor()
-    cursor.execute(f'SELECT Critique.ramen_id \
+    cursor.execute(f'SELECT Critique.ramen_id, Critique.rating, Critique.ramen_id, Product.name \
     FROM Critic \
     JOIN Critique ON (Critic.critic_id = Critique.critic_id) \
-    WHERE Critic.critic_id = {critic_id}')
-    row_headers = [x[0] for x in cursor.description]
-    json_data = []
-    theData = cursor.fetchall()
-    for row in theData:
-        json_data.append(dict(zip(row_headers, row)))
-    the_response = make_response(jsonify(json_data))
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-    return the_response
-
-@CriticHome.route('/highest_ratings/<critic_id>', methods=["GET"])
-def get_highest_ratings(critic_id):
-    cursor = db.get_db().cursor()
-    cursor.execute(f'SELECT Critique.ramen_id \
-    FROM Critic \
-    JOIN Critique ON (Critic.critic_id = Critique.critic_id) \
+    JOIN Product on (Product.ramen_id = Critique.ramen_id) \
     WHERE Critic.critic_id = {critic_id} \
     ORDER BY Critique.rating DESC')
     row_headers = [x[0] for x in cursor.description]
